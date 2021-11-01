1888b1ed53d2f9cbd8abf9ff91b031e1b94d7c64
Dieisileo <dieisileo@gmail.com>
2021-11-01 16:10:38 -0300

    search

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import { FiSearch } from "react-icons/fi";
 import './styles/global.css'
 
 function App() {
+  const [books, setBooks] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+  async function handleSearch() {
+    try {
+      const {data} = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${inputValue}`);
+      setBooks(data.itens);
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <div>
       <div className="title">
         <h1>Book Finder</h1>
       </div>
 
-      <form action="">
-        <input type="text"
-        placeholder="Pesquise por título ou autor" />
+      <form action="" onSubmit={handleSearch}>
+        <input 
+        type="text"
+        placeholder="Pesquise por título ou autor"
+        onChange={e => setInputValue(e.target.value)}
+        />
 
-        <button>
+        <button type="submit">
         <FiSearch id="iconSearch"/>
         </button>
       </form>
