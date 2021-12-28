2a404cf6b9877ba2eba4403fed1f74b1ef6c279e
Dieisileo <dieisileo@gmail.com>
2021-12-28 13:30:53 -0300

    button and progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
+import { CircularProgress } from '@material-ui/core'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { FiSearch } from 'react-icons/fi'
+import { FiSearch, FiX } from 'react-icons/fi'
 import { CardBook } from './components/CardBook'
 import './styles/global.css'
 
 function App() {
   const [books, setBooks] = useState()
   const [inputValue, setInputValue] = useState('')
+  const [loading, setLoading] = useState(false)
 
   async function handleSearch(e) {
     e.preventDefault()
     try {
+
+      setLoading(true)
+
       const { data } = await axios.get(
         `https://www.googleapis.com/books/v1/volumes?q=${inputValue}`
-      ) 
-      console.log(data.itens)
+      )
       setBooks(data.items)
+
+      setLoading(false)
+
     } catch (err) {
       console.error(err)
     }
   }
 
   useEffect(() => {
-    console.log(books)
-  },[books])
+    console.log(loading)
+  }, [loading])
 
   return (
     <div>
@@ -41,11 +48,23 @@ function App() {
         <button type="submit">
           <FiSearch id="iconSearch" />
         </button>
+
+        {inputValue.length >= 1 && (
+          <button type="reset" id="buttonClear">
+            <FiX id="iconClear" />
+          </button>
+        )
+        }
       </form>
+      <div className='boxLoading'>
+        {loading && (
+          <CircularProgress />
+        )}
+      </div>
       <div className="resultsBooks">
-      {!!books && books.map(book => (
-        <CardBook key={book.id} book={book} />
-      ))}
+        {!!books && books.map(book => (
+          <CardBook key={book.id} book={book} />
+        ))}
       </div>
     </div>
   )
